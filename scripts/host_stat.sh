eece68e63f6ce1b967f1383ecc610d13e530c2e4
Derrick Yeung <lscspirit@hotmail.com>
2013-08-11 17:16:12 +0800

    A simple bash script for query host CPU and memory usage

---
+++ scripts/host_stat.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+UNAME=`uname`
+
+case "$UNAME" in
+  Darwin) MEM_STAT=`top -l 1 | sed -En 's/PhysMem:.*[^0-9\.]+([0-9\.]+)M used, ([0-9\.]+)M free\./\1 \2/gp' | awk '{ total = $1 + $2; print "mem_used=" $1 ",mem_free=" $2 ",mem_total=" total; }'`
+          CPU_STAT=`top -l 1 | sed -En 's/CPU usage:.*[^0-9\.]+([0-9\.]+)% idle/\1/gp' | awk '{ cpu_used = 100 - $1; print "cpu_used=" cpu_used ",cpu_free=" $1 }'`
+          ;;
+  Linux)  MEM_STAT=`free -m | sed -rn 's/(Mem|Swap):[^0-9]*([0-9]+)[^0-9]*([0-9]+).*/\1 \2 \3/gp' | awk '{ used = $2 - $3; print $1"_used="used ","$1"_free="$3 ","$1"_total="$2 }'`
+          CPU_STAT=`mpstat | sed -rn 's/.*all.*[^0-9\.]+([0-9\.]+)[^0-9\.]+[0-9\.]+/\1/gp' | awk '{ cpu_used = 100 - $1; print "cpu_used=" cpu_used ",cpu_free=" $1 }'`
+          ;;
+esac
+
+echo $CPU_STAT","${MEM_STAT//$'\n'/,}
+
